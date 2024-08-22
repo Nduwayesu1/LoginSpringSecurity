27009a08e3b3aaa28e8e77876c61e7dd86945f1d
Nduwayesu1 <99731288+Nduwayesu1@users.noreply.github.com>
2024-08-22 16:40:49 +0200

    Update userServiceImpl.java

--- userServiceImpl.java
+++ userServiceImpl.java
@@ -16,26 +16,13 @@ public class userServiceImpl implements userService {
     @Override
     public User signUp(User users) {
         // Example: Perform validation or additional logic before saving
-        User user= new User(null,users.getFull_name(),users.getEmail(),passwordEncoder.encode(users.getPassword()),users.getRole());
+        User user= new User(null,users.getFull_name(),users.getUsername(),passwordEncoder.encode(users.getPassword()),users.getRole());
         return userRepo.save(user);
     }
 
-    @Override
-    public User login(String email, String password, String role) {
-        User existingUser = findByEmail(email);
-
-        if (existingUser != null &&
-                passwordEncoder.matches(password, existingUser.getPassword()) &&
-                existingUser.getRole().equals(role)) {
-            return   existingUser;// Return the authenticated user
-        }
-
-
-        return null; // Return null if no user found or credentials don't match
-    }
 
     @Override
-    public User findByEmail(String email) {
-        return userRepo.findByEmail(email);
+    public User findByUsername(String username) {
+        return userRepo.findByUsername(username);
     }
 }
